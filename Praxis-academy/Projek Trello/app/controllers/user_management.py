2c2a47194e60bd9c12d2b39ee54a67d3fb255acf
Reza-Irvando <rezairvando@mail.ugm.ac.id>
2022-11-21 22:31:12 +0700

    Tambah Projek Iboost

--- Praxis-academy/Projek Trello/app/controllers/user_management.py
+++ Praxis-academy/Projek Trello/app/controllers/user_management.py
@@ -1,15 +1,15 @@
 from flask import request
 from app import validators
 from http import HTTPStatus
-from app import response
+from app import responses
 from app import models
 from bson import ObjectId
 
-def AddList():
+def Regist():
     bodyJson = request.json
     err = validators.AddList(bodyJson)
     if err:
-        return response.Make(
+        return responses.Make(
             Status = HTTPStatus.BAD_REQUEST.value,
             Message = "error",
             Data = str(err)
@@ -19,13 +19,13 @@ def AddList():
         ToDoList = bodyJson["ToDoList"], Deskripsi = bodyJson["Deskripsi"])
     
     collList.save()
-    return response.Make(
+    return responses.Make(
         Status = HTTPStatus.OK.value,
         Message = "success",
         Data = f"To Do List {bodyJson['ToDoList']} has added"
     ), HTTPStatus.OK.value
 
-def ViewList():
+def List():
     collList = models.List.objects().all()
     data = []
     for i in collList:
@@ -35,19 +35,19 @@ def ViewList():
             "Deskripsi": i.Deskripsi,
         })
     print(collList)
-    return response.Make(
+    return responses.Make(
         Status=HTTPStatus.OK.value,
         Message="success",
         Data=data
     ), HTTPStatus.OK.value
 
-def UpdateList():
+def Analytic():
     listId = request.args["id"]
     print(listId)
     bodyJson = request.json
     err = validators.UpdateList(bodyJson)
     if err:
-        return response.Make(
+        return responses.Make(
             Status = HTTPStatus.BAD_REQUEST.value,
             Message = "error",
             Data = str(err)
@@ -57,31 +57,67 @@ def UpdateList():
     print(collList)
     return(bodyJson)
 
-def DeleteList():
+def Topup():
     listId = request.args["id"]
     collList = models.List.objects(id = ObjectId(listId)).delete()
     if not collList:
-        return response.Make(
-            Status=HTTPStatus.BAD_REQUEST.value,
-            Message="error",
-            Data="Tidak Ditemukan."
-        ), HTTPStatus.BAD_REQUEST.value
-    return response.Make(
-        Status = HTTPStatus.OK.value,
-        Message = "success",
-        Data = f"successfuly deleted list : {listId}"
-    ), HTTPStatus.OK.value
-
-def DetailList():
-    listId = request.args["id"]
-    collList = models.List.objects(id = ObjectId(listId)).first()
-    if not collList:
-        return response.Make(
+        return responses.Make(
             Status=HTTPStatus.OK.value,
             Message="error",
             Data="Tidak Ditemukan."
         ), HTTPStatus.BAD_REQUEST.value
-    return response.Make(
+    return responses.Make(
+        Status = HTTPStatus.BAD_REQUEST.value,
+        Message = "success",
+        Data = f"successfuly deleted list : {listId}"
+    ), HTTPStatus.OK.value
+
+def Split():
+    listId = request.args["id"]
+    collList = models.List.objects(id = ObjectId(listId)).first()
+    if not collList:
+        return responses.Make(
+            Status=HTTPStatus.OK.value,
+            Message="error",
+            Data="Tidak Ditemukan."
+        ), HTTPStatus.BAD_REQUEST.value
+    return responses.Make(
+        Status=HTTPStatus.OK.value,
+        Message="success",
+        Data={
+            "ToDoList": collList.ToDoList,
+            "Deskripsi": collList.Deskripsi
+        }
+    ), HTTPStatus.OK.value
+
+def Approval():
+    listId = request.args["id"]
+    collList = models.List.objects(id = ObjectId(listId)).first()
+    if not collList:
+        return responses.Make(
+            Status=HTTPStatus.OK.value,
+            Message="error",
+            Data="Tidak Ditemukan."
+        ), HTTPStatus.BAD_REQUEST.value
+    return responses.Make(
+        Status=HTTPStatus.OK.value,
+        Message="success",
+        Data={
+            "ToDoList": collList.ToDoList,
+            "Deskripsi": collList.Deskripsi
+        }
+    ), HTTPStatus.OK.value
+
+def History():
+    listId = request.args["id"]
+    collList = models.List.objects(id = ObjectId(listId)).first()
+    if not collList:
+        return responses.Make(
+            Status=HTTPStatus.OK.value,
+            Message="error",
+            Data="Tidak Ditemukan."
+        ), HTTPStatus.BAD_REQUEST.value
+    return responses.Make(
         Status=HTTPStatus.OK.value,
         Message="success",
         Data={
---
+++ Praxis-academy/Projek Trello/app/validators/user_management.py
@@ -0,0 +1,24 @@
+from json_checker import Checker
+
+def Regist(request):
+    try:
+        schema = {
+            "ToDoList": str,
+            "Deskripsi": str
+        }
+        checker = Checker(schema)
+        checker.validate(request)
+        return False 
+    except Exception as err:
+        return err
+
+def UpdateList(data):
+    try:
+        checker = Checker({
+            "ToDoList": str,
+            "Deskripsi": str
+        })
+        checker.validate(data)
+        return False
+    except Exception as err:
+        return err
