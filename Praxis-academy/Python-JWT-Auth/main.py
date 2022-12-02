5c817b18b53d4fe208165a1fa4349c77add5716a
Reza-Irvando <rezairvando@mail.ugm.ac.id>
2022-12-02 18:22:41 +0700

    Tambah Python-JWT-Auth

---
+++ Praxis-academy/Python-JWT-Auth/main.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from os import getenv
+from datetime import timedelta
+import redis
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = getenv('SECRET_KEY')
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = getenv('ACCESS_EXPIRES')
+
+jwt = JWTManager(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    payload = request.json
+    user = {
+        'id': 2,
+        'username': 'mata',
+        'password': 'we'
+    }
+    if payload['username'] == user['username'] and payload['password'] == user['password']:
+        return jsonify({
+            'status': 200,
+            'data': {
+                'access_token': create_access_token(identity=user)
+            },
+            'message': 'success'
+        }), 200
+    else:
+        return jsonify({
+            'status': 400,
+            'data': 'periksa username dan password anda',
+            'message': 'error'
+        }), 400
+
+@app.route('/user')
+@jwt_required()
+def user():
+
+    return jsonify({
+        'status': 200,
+        'data': get_jwt_identity(),
+        'message': 'success'
+    })
+
+@app.route('/')
+def index():
+    return jsonify({
+        'status': 200,
+        'data': '',
+        'message': 'success'
+    })
